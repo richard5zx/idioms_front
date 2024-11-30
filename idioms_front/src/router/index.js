2dce58944f1cbd8fc0f14dc02dbf3ba40236f60b
richard5zx <richardchao2009@gmail.com>
2024-11-30 23:52:36 +0800

    debug router js file

--- idioms_front/src/router/index.js
+++ idioms_front/src/router/index.js
@@ -2,7 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 import AddIdiom from '../components/idiom/AddIdiom.vue';
 import SearchIdiom from '../components/idiom/SearchIdiom.vue';
 import UpdateIdiom from '../components/idiom/UpdateIdiom.vue';
-import DeleteIdiom from '../components/idiom/AddIdiom.vue';
+import DeleteIdiom from '../components/idiom/DeleteIdiom.vue';
 import AddExample from '../components/idiom_example/AddExample.vue';
 import SearchExample from '../components/idiom_example/SearchExample.vue';
 import UpdateExample from '../components/idiom_example/UpdateExample.vue';
