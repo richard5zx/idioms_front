da8bec4dbf71bdd4462a7487c1984132db195c96
richard5zx <richardchao2009@gmail.com>
2025-01-07 11:43:53 +0800

    delete Idiom vue done

--- idioms_front/src/components/idiom/deleteIdiom.vue
+++ idioms_front/src/components/idiom/deleteIdiom.vue
@@ -1,11 +1,51 @@
 <template>
-    Delete Idiom
+    <table width="40%" border="1">
+        <tr>
+            <td colspan="2" align="center">Delete Idiom</td>
+        </tr>
+        <tr>
+            <td>Idiom Id</td>
+            <td><input type="text" v-model="idiomId"></td>
+        </tr>
+        <tr>
+            <td colspan=2 align="center">
+                <button @click="deleteIdiom()">Delete Idiom</button>
+            </td>
+        </tr>
+    </table>
 </template>
 
 <script>
+import axios from 'axios'
     export default {
-        name:'DeleteIdiom'
+        name:'DeleteIdiom',
+        
+        data() {
+            return {
+                idiomId: ''
+            }
+        },
+        methods: {
+            deleteIdiom() {
+                // Subject to change
+                var url = "http://192.168.100.92:8080/idiom/deleteIdiom";
+                axios
+                    .delete(url + "/" + this.idiomId)
+                    .then(response => this.message = response.data)
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                var message = "Idiom Id:" + this.idiomId + " is deleted"
+                alert(message);
+                this.clearTable();
+            },
+            clearTable() {
+                this.idiomId = '';
+            },
+        }
     }
 </script>
 
-<style></style>
+<style>
+
+</style>
